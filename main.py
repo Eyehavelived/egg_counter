90bfdb48748ed62735bab85bab4f2b8805c8c663
Jay Tan <email.jingjie@gmail.com>
2021-08-08 02:00:11 +1000

    Set button to run blob method as test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from blob_counter import blob_method
 
 class App(tk.Frame):
     def __init__(self, master):
@@ -24,7 +25,7 @@ class App(tk.Frame):
         self.entrythingy.bind('<Key-Return>',
                               self.print_contents)
 
-        button1 = tk.Button(text='Click Me', command=self.print_contents, bg='brown', fg='white')
+        button1 = tk.Button(text='Click Me', command=self.run_blob_test, bg='brown', fg='white')
         self.canvas1.create_window(150, 150, window=button1)
 
     # def hello(self):
@@ -34,6 +35,36 @@ class App(tk.Frame):
     def print_contents(self):
         print("Hi. The current entry content is:",
               self.contents.get())
+
+    def run_blob_test(self):
+        # IO directories
+        input_dir = 'input'
+        output_dir = 'output'
+
+        # crop amount
+        h_crop = 0
+        w_crop = 100
+
+        # contrast adjustment
+        contrast = 3
+        brightness = -350
+
+        # plate circle params
+        circ_param1 = 50
+        circ_param2 = 30
+        circ_min = 450
+        circ_max = 550
+        circ_dist = 1000
+
+        # blob params
+        marker_size = 25
+        min_circularity = 0.3
+        min_convex = 0.4
+        min_inertia = 0.05
+        min_area = 500
+
+        blob_method(input_dir, output_dir, h_crop, w_crop, contrast, brightness, circ_param1, circ_param2, circ_min,
+                    circ_max, circ_dist, marker_size, min_circularity, min_convex, min_inertia, min_area)
 
 if __name__ == '__main__':
     root = tk.Tk()
